5c4dfd99c894e8b1cf096bc37572ec227e0f2bfc
Nitzan Bueno <nitzan.bueno@gmail.com>
2016-12-30 21:18:08 +0200

    Add turn checking and reconnect support

--- server/index.js
+++ server/index.js
@@ -21,37 +21,113 @@ var server = app.listen(process.env.PORT, function () {
   console.log('Server running...');
 });
 
+function setId(m, user) {
+  var id = user.id.substring(0, 5);
+  IDs[id] = user;
+  user.message("setId", id);
+}
+
 cloak.configure({
   express: server,
   defaultRoomSize: 2,
+  pruneEmptyRooms: 2000,
   messages: {
     joinRoom: function (id, user) {
-      var mate = IDs[id];
+      // User requested id, so their mate is IDs[id]
+      if (id in IDs) {
+        var mate = IDs[id];
+      }
+      // If the requested mate doesn't exist, 
+      // we're just gonna act as if there was no request and set ID
+      else {
+        setId(id, user);
+        return;
+      }
       var newRoom = cloak.createRoom(id);
       delete IDs[id];
       user.data.mate = mate;
       mate.data.mate = user;
       newRoom.addMember(user);
       newRoom.addMember(mate);
-      var chaos = Math.random() >= 0.5;
-      if (chaos) {
-        user.message("completed", "chaos");
-        mate.message("completed", "order");
+      // If true, then 'user' is chaos, else 'user' is order
+      var chaos;
+      // If mate is defined to be chaos (by previous connect) than that takes hold
+      if ('chaos' in mate.data) {
+        chaos = !mate.data.chaos;
       } else {
-        mate.message("completed", "chaos");
-        user.message("completed", "order");
+        // Else just randomly choose
+        chaos = Math.random() >= 0.5;
       }
+      
+      
+      // Messages are in the form "<order/chaos>,<turn/wait>"
+      // with "order/chaos" meaning the role of the player, and "turn/wait" meaning who goes first
+      // The message sent to the "hosting" user
+      var userMessage;
+      // The message sent to the mate
+      var mateMessage;
+
+      if (chaos) {
+        userMessage = "chaos";
+        user.data.chaos = true;
+        mateMessage = "order";
+        mate.data.chaos = false;
+      } else {
+        mateMessage = "chaos";
+        mate.data.chaos = true;
+        userMessage = "order";
+        user.data.chaos = false;
+      }
+      
+      // Similarly with the turn
+      var turn = Math.random() >= 0.5;
+      if ('turn' in mate.data) {
+        turn = !mate.data.turn;
+      }
+      if (turn) {
+        userMessage += ",turn";
+        user.data.turn = true;
+        mateMessage += ",wait";
+        mate.data.turn = false;
+      } else {
+        mateMessage += ",turn";
+        mate.data.turn = true;
+        userMessage += ",wait";
+        user.data.turn = false;
+      }
+      
+      user.message("completed", userMessage);
+      mate.message("completed", mateMessage);
     },
-    initRoom: function (message, user) {
-      var id = user.id.substring(0, 5);
-      IDs[id] = user;
-      user.message("setId", id);
-    },
+    initRoom: setId,
     putTile: function (message, user) {
       user.data.mate.message("tile", message);
+      // putTile means the user has just played so it's now not their turn
+      user.data.turn = false;
+      user.data.mate.data.turn = true;
+    },
+    sendTile: function (message, user) {
+      user.data.mate.message("blindTile", message);
     },
     clearBoard: function (message, user) {
       user.data.mate.message("clear", message);
+    }
+  },
+  clientEvents: {
+    disconnect: function (user) {
+      // Hopefully the user has a mate, if not the room shall disintegrate
+      var mate = user.data.mate;
+      try {
+        // Let the connected user (if exists) know their opponent has disconnected
+        if (mate.connected()) {
+          mate.message("disconnected", "");
+          // Then allow for the option to reconnect
+          setId(null, mate);
+        }
+      } catch (err) {
+        // Mate probably doesn't exist or something, I dunno
+        // Anyways, just do nothing
+      }
     }
   }
 });
--- site/game.js
+++ site/game.js
@@ -5,6 +5,8 @@ var running = false;
 var width = 6;
 var height = 6;
 var amOrder = false;
+// Declare the board
+var board;
 
 function enabledPlay() {
   $("#showme").html("");
@@ -19,11 +21,20 @@ function addTile(cell, type, color) {
       cell.addClass("marked");
       if (type == "o") {
         cell.append('<img class="mark" src="/O_' + color + '.png">');
-        return true;
       } else {
         cell.append('<img class="mark" src="/X_' + color + '.png">');
-        return true;
       }
+      var x = parseInt(cell.attr("x"), 10);
+      var y = parseInt(cell.attr("y"), 10);
+      // mark shall be 'x' or 'o', lowercase if it's order, and uppercase if it's chaos
+      var mark;
+      if (color == "red") {
+        mark = type.toUpperCase();
+      } else {
+        mark = type.toLowerCase();
+      }
+      board[x][y] = mark;
+      return true;
     }
   }
   return false;
@@ -52,14 +63,69 @@ function getColor(me) {
 function clearBoard() {
   $("table.board td").html("");
   $("table.board td").removeClass("marked");
+  board = new Array(width);
+  for (var i = 0; i < width; i++) {
+    board[i] = new Array(height);
+  }
 }
+
+function canPlay() {
+  return turn || localPlay;
+}
+
+function updateTurn(newTurn) {
+  turn = newTurn;
+  if (localPlay) {
+    if (turn) {
+      $("#turn").html("It is now <strong style=\"color:" + getColor() + "\">Chaos's</strong> turn.");
+    } else {
+      $("#turn").html("It is now <strong style=\"color:" + getColor() + "\">Order's</strong> turn.");
+    }
+  } else {
+    if (turn) {
+      $("#turn").html("It is now <strong>your</strong> turn.");
+    } else {
+      $("#turn").html("It is now <strong>your opponent's</strong> turn.");
+    }
+  }
+}
+
+// Reset all HTML elements (except showme) to their initial state
+function resetPage() {
+  $("#friendlink").html("Loading...");
+  $("#turn").html("");
+  $("#localButton").show();
+  $("#clearButton").hide();
+  enablePlay = false;
+}
+
+// Send every tile that's currently on my board to the opponent via addTile.
+// Useful when opponent is reconnecting
+function sendBoard() {
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+      if (board[x][y]) {
+        var mark = board[x][y];
+        var type = mark.toLowerCase();
+        // mark == type means mark is lowercase, so it's order. Else it's chaos.
+        var color = mark == type ? "blue" : "red";
+        if (mark) {
+          var mess = x + "," + y + "," + type + "," + color;
+          console.log(mess);
+          cloak.message("sendTile", mess);
+        }
+      }
+    }
+  }
+}
+
 var userID = "original";
 $(function () {
   // Generate Table
   var tablestr = "";
-  for (y = 0; y < height; y++) {
+  for (var y = 0; y < height; y++) {
     tablestr += "<tr>";
-    for (x = 0; x < width; x++) {
+    for (var x = 0; x < width; x++) {
       tablestr += "<td x='" + x + "' y='" + y + "'></td>";
     }
     tablestr += "</tr>";
@@ -67,23 +133,27 @@ $(function () {
   $("#board").append(tablestr);
   $("table.board td:not(.marked)").on({
     mousedown: function (event) {
-      var type = "x";
-      var cell = $(this);
-      if (event.which == 3) {
-        type = "o";
-      }
-      var color = getColor(true);
-      if (addTile(cell, type, color)) {
-        turn = !turn;
-        cloak.message("putTile", cell.attr("x") + "," + cell.attr("y") + "," + type);
+      if (canPlay()) {
+        var type = "x";
+        var cell = $(this);
+        if (event.which == 3) {
+          type = "o";
+        }
+        var color = getColor(true);
+        if (addTile(cell, type, color)) {
+          updateTurn(!turn);
+          cloak.message("putTile", cell.attr("x") + "," + cell.attr("y") + "," + type);
+        }
       }
     },
     contextmenu: function () {
       return false;
     }
   });
+  clearBoard();
   $("#localButton").click(function () {
     localPlay = true;
+    clearBoard();
     if (running) {
       cloak.end();
       running = false;
@@ -113,23 +183,40 @@ $(function () {
         $("#friendlink").html(window.location.protocol + "//" + window.location.hostname + "/p/" + id);
       },
       completed: function (message) {
-        playerType = "neither type because there was something wrong lol";
-        if (message == "order") {
-          playerType = "Order";
+        data = message.split(",");
+        // The role of the player (order or chaos)
+        role = data[0];
+        if (role == "order") {
+          role = "Order";
           amOrder = true;
-        } else if (message == "chaos") {
-          playerType = "Chaos";
+        } else if (role == "chaos") {
+          role = "Chaos";
           amOrder = false;
         }
-        $("#friendlink").html("You are connected.<br><small>You are playing as <strong style=\"color: " + getColor(true) + "\">" + playerType + "</strong>.</small>");
+        // The turn is the second part of the message
+        updateTurn(data[1] == "turn");
+        $("#friendlink").html("You are connected.<br><small>You are playing as <strong style=\"color: " + getColor(true) + "\">" + role + "</strong>.</small>");
+        sendBoard();
         enabledPlay();
       },
       tile: function (tiledata) {
+        // tile means the opponent has played a tile
+        updateTurn(true);
         var data = tiledata.split(",");
         addTile($("td[x='" + data[0] + "'][y='" + data[1] + "']"), data[2], getColor(false));
       },
+      blindTile: function (tiledata) {
+        // blindTile means opponent is sending a tile but not through playing.
+        // When a board gets sent via reconnection this gets called
+        var data = tiledata.split(",");
+        addTile($("td[x='" + data[0] + "'][y='" + data[1] + "']"), data[2], data[3]);
+      },
       clear: function (message) {
         clearBoard();
+      },
+      disconnected: function (message) {
+        $("#showme").html("You opponent has disconnected. <br>To reconnect, they should go to this URL:");
+        resetPage();
       }
     }
   });
